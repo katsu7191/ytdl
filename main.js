8704bd89954bfa6555d0f5f11dcf332b4651ea73
katsu7191 <65995836+katsu7191@users.noreply.github.com>
2021-05-10 10:57:33 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 const data = require("./data.json")
-const BASE_PATH = `https://www.youtube.com/watch?v=`;
+const url = `https://www.youtube.com/watch?v=` + data.yturl;
 const youtubeId = data.yturl;
 const url = BASE_PATH+youtubeId;
+const ytdl = require('ytdl-core');
+const pathToFfmpeg = require('ffmpeg-static');
+const {exec} = require('child_process');
 
-ytdl(url).pipe(fs.createWriteStream(`${youtubeId}-Blackrose-YouTube-Downloader.mp4`));
+var title;
+
+ytdl.getInfo(url,{downloadURL:true},function(err,info){
+if(err)throw err;
+
+console.log('動画タイトル');
+title=info.player_response.videoDetails.title
+console.log(title);
+
+download();
+});
+
+function download(){ytdl(url)
+.pipe(fs.createWriteStream(`‪${title}.mp4`))
+.on('close',function(){
+exec(`ffmpeg -i "‪${title}.mp4" "‪${title}.mp3"`);
+});
+}
